5e1a7766a4fed5b136e1d27b9f421098335fd811
SantaRenata <renatasanta2002@gmail.com>
2023-03-27 09:19:22 +0200

    Kezdés

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const[side,setSide] = useState('');
+  const[alpha,setAlpha] = useState('');
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>
+        TOMBSUG
+      </Text>
+
+  
+      <View style={styles.body}>
+        <Text>Rombuszba írható kör sugara</Text>
+
+        <Text>Oldal:</Text>
+        <TextInput style={styles.input}
+        onChangeText={sideInput => setSide(sideInput)}
+
+        />
+
+        <Text>Alfa szög:</Text>
+        <TextInput style={styles.input}
+        onChangeText={alphaInput => setAlpha(alphaInput)}
+        />
+
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    backgroundColor:'darksalmon',
+    borderRadius:4,
+    width:'100%',
+    height:40,
+    textAlign:'center',
+    fontSize:30
+  },
+  input:{
+    backgroundColor:'white',
+    width:'20%',
+    height:30
+  },
+  body:{
+    flex:1,
+    width:'100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
